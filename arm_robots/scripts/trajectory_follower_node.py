da42fb415c79a75241d504c8fca51f08df7168ab
Tom <tpower@umich.edu>
2020-10-27 17:05:02 -0400

    use argparse with rospy for argument parsing

--- arm_robots/scripts/trajectory_follower_node.py
+++ arm_robots/scripts/trajectory_follower_node.py
@@ -2,29 +2,32 @@
 
 import numpy as np
 
+import sys
 import rospy
+import argparse
 from arm_robots.hdt_michigan import BaseVal
 from arm_robots.trajectory_follower import TrajectoryFollower
 from arm_robots.victor import BaseVictor
+
+
 
 
 def main():
     np.set_printoptions(linewidth=200, suppress=True, precision=4)
     rospy.init_node('trajectory_follower')
 
-    robot_name_rosparam_name = "~robot_name"
-    if robot_name := rospy.get_param(robot_name_rosparam_name, None):
-        if robot_name == "victor":
-            base_robot = BaseVictor(robot_name)
-        elif robot_name == "victor":
-            base_robot = BaseVal(robot_name)
-        else:
-            raise NotImplementedError(f"Invalid ros param {robot_name_rosparam_name} {robot_name}")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("robot_name", type=str)
+    parser.add_argument("controller_name", type=str)
+    args = parser.parse_args(args=rospy.myargv(argv=sys.argv)[1:])
+
+    if args.robot_name == "victor":
+        base_robot = BaseVictor(args.robot_name)
+    elif args.robot_name == "val":
+        base_robot = BaseVal(args.robot_name)
     else:
-        rospy.loginfo(f"rosparam {robot_name_rosparam_name} not set, Defaulting to Victor")
-        base_robot = BaseVictor(robot_name)
-    controller_name = rospy.get_param('~controller')
-    fwd = TrajectoryFollower(base_robot, controller_name)
+        raise NotImplementedError(f"Invalid robot name {args.robot_name}")
+    fwd = TrajectoryFollower(base_robot, args.controller_name)
     fwd.start_server()
 
     rospy.spin()
