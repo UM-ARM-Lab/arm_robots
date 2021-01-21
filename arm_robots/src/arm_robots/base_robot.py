7a141d92189de4963fbc02dcfa82825e20c50ee3
Mark Van der Merwe <markvdm@umich.edu>
2021-01-21 15:48:01 -0500

    Add new BaseRobot abstract class for single arm setup.
    Not addressing how to get back to DualArmRobot.

--- arm_robots/src/arm_robots/base_robot.py
+++ arm_robots/src/arm_robots/base_robot.py
@@ -10,7 +10,65 @@ from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectoryPoint
 
 
+class BaseRobot:
+
+    def __init__(self, robot_namespace: str = ''):
+        """
+        Single arm version of DualArmRobot.
+        For now keeping grippers out of this. 
+
+        TODO: Grippers setup.
+        """
+        self.robot_namespace = robot_namespace
+        # the robot namespace will be prepended by setting ROS_NAMESPACE environment variable or the ns="" in roslaunch
+        joint_states_topic = ns_join(self.robot_namespace, 'joint_states')
+        self._joint_state_listener = Listener(joint_states_topic, JointState)
+
+        self.tf_Wrapper = TF2Wrapper()
+
+        try:
+            self.robot_commander = moveit_commander.RobotCommander(ns=self.robot_namespace)
+        except RuntimeError as e:
+            rospy.logerr("You may need to load the moveit planning context and robot description")
+            print(e)
+
+    def ns(self, name: str):
+        return ns_join(self.robot_namespace, name)
+
+    def send_joint_command(self, joint_names: List[str], trajectory_point: JointTrajectoryPoint) -> Tuple[bool, str]:
+        raise NotImplementedError()
+
+    def get_joint_positions(self, joint_names: Optional[List[str]] = None):
+        """
+        :args joint_names an optional list of names if you want to have a specific order or a subset
+        """
+        joint_state: JointState = self._joint_state_listener.get()
+        if joint_names is None:
+            return joint_state.position
+
+        current_joint_positions = []
+        for name in joint_names:
+            if name not in joint_state.name:
+                ros_helpers.logfatal(ValueError, f"Joint {name} not found in joint states")
+            idx = joint_state.name.index(name)
+            pos = joint_state.position[idx]
+            current_joint_positions.append(pos)
+        return current_joint_positions
+
+    def check_inputs(self, group_name: str, ee_link_name: str):
+        links = self.robot_commander.get_link_names()
+        if ee_link_name not in links:
+            rospy.logwarn_throttle(1, f"Link [{ee_link_name}] does not exist. Existing links are:")
+            rospy.logwarn_throttle(1, links)
+
+        groups = self.robot_commander.get_group_names()
+        if group_name not in groups:
+            rospy.logwarn_throttle(1, f"Group [{group_name}] does not exist. Existing groups are:")
+            rospy.logwarn_throttle(1, groups)
+
+# TODO: Extend DualArmRobot from BaseRobot?
 class DualArmRobot:
+    
     def __init__(self, robot_namespace: str = ''):
         """
         This class is designed around the needs of the trajectory_follower.TrajectoryFollower
