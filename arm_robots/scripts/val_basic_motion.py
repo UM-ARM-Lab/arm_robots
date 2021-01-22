8dc9d03511dee05c225747e520a3e0c3cc0050c1
Peter <peter@armnova>
2021-01-22 08:20:39 -0500

    comment back in

--- arm_robots/scripts/val_basic_motion.py
+++ arm_robots/scripts/val_basic_motion.py
@@ -25,59 +25,45 @@ def main():
     val = Val(raise_on_failure=True)
     val.connect()
 
-    # val.open_left_gripper()
-    # val.close_left_gripper()
-    # val.open_right_gripper()
-    # val.close_right_gripper()
-    #
-    # print("press enter if prompted")
-    #
-    # # Plan to joint config
-    # myinput("Plan to home joint config?")
-    # val.plan_to_joint_config('both_arms', 'home')
-    #
-    # # s = rospy.ServiceProxy("hdt_michigan/plan_kinematic_path", GetMotionPlan)
-    # # req = GetMotionPlanRequest()
-    # # right_tool_goal = Constraints()
-    # # right_tool_goal.name = 'right_tool_goal'
-    # # right_tool_goal_position = PositionConstraint(link_name='right_tool', target_point_offset=Vector3(x=0.3, y=0.6, z=0.5))
-    # # right_tool_goal_position.header.frame_id = 'robot_root'
-    # # right_tool_goal_position.weight = 1.0
-    # # right_tool_goal.position_constraints = [right_tool_goal_position]
-    # # req.motion_plan_request.goal_constraints = [right_tool_goal]
-    # # req.motion_plan_request.start_state.
-    # # req.motion_plan_request.group_name = 'both_arms'
-    # # s(req)
-    #
-    # # Plan to pose
-    # myinput("Plan to pose 1?")
-    # val.plan_to_pose(val.right_arm_group, val.right_tool_name, [0.3, 0.8, 0.5, -np.pi / 2, np.pi / 2, 0])
-    #
-    # # Or you can use a geometry msgs Pose
-    # myinput("Plan to pose 2?")
-    # pose = Pose()
-    # pose.position.x = 0.2
-    # pose.position.y = 0.5
-    # pose.position.z = 0.1
-    # q = quaternion_from_euler(-np.pi, 0, 0)
-    # pose.orientation.x = q[0]
-    # pose.orientation.y = q[1]
-    # pose.orientation.z = q[2]
-    # pose.orientation.w = q[3]
-    # val.plan_to_pose(val.right_arm_group, val.right_tool_name, pose)
-    #
-    # # or with cartesian planning
-    # myinput("cartesian motion to pose 3?")
-    # val.plan_to_position_cartesian(val.right_arm_group, val.right_tool_name, [0.4, 0.3, 0.1], step_size=0.01)
-    #
-    # myinput("follow jacobian to pose 4?")
-    # val.store_current_tool_orientations([val.right_tool_name])
-    # val.follow_jacobian_to_position('both_arms', [val.right_tool_name], [[[0.3, 0.7, 0.3]]], vel_scaling=1.0)
-    # val.follow_jacobian_to_position('both_arms', [val.right_tool_name], [[[0.3, 0.3, 0.3]]], vel_scaling=1.0)
+    val.open_left_gripper()
+    val.close_left_gripper()
+    val.open_right_gripper()
+    val.close_right_gripper()
+
+    print("press enter if prompted")
+
+    # Plan to joint config
+    myinput("Plan to home joint config?")
+    val.plan_to_joint_config('both_arms', 'home')
+
+    # Plan to pose
+    myinput("Plan to pose 1?")
+    val.plan_to_pose(val.right_arm_group, val.right_tool_name, [0.3, 0.8, 0.5, -np.pi / 2, np.pi / 2, 0])
+
+    # Or you can use a geometry msgs Pose
+    myinput("Plan to pose 2?")
+    pose = Pose()
+    pose.position.x = 0.2
+    pose.position.y = 0.5
+    pose.position.z = 0.1
+    q = quaternion_from_euler(-np.pi, 0, 0)
+    pose.orientation.x = q[0]
+    pose.orientation.y = q[1]
+    pose.orientation.z = q[2]
+    pose.orientation.w = q[3]
+    val.plan_to_pose(val.right_arm_group, val.right_tool_name, pose)
+
+    # or with cartesian planning
+    myinput("cartesian motion to pose 3?")
+    val.plan_to_position_cartesian(val.right_arm_group, val.right_tool_name, [0.4, 0.3, 0.1], step_size=0.01)
+
+    myinput("follow jacobian to pose 4?")
+    val.store_current_tool_orientations([val.right_tool_name])
+    val.follow_jacobian_to_position('both_arms', [val.right_tool_name], [[[0.3, 0.5, 0.3]]], vel_scaling=1.0)
 
     val.disconnect()
     ros_init.shutdown()
 
 
-if __name__ == "__main__":
-    main()
+ __name__ == "__main__":
+  main()
