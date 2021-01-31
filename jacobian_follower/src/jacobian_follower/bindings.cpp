fa2a817d6c94d5b368811a7749159c1a14ba00cc
Peter Mitrano <pmitrano@armstorm>
2021-01-30 21:08:53 -0500

    Allow jacobian-based planning given a start state

--- jacobian_follower/src/jacobian_follower/bindings.cpp
+++ jacobian_follower/src/jacobian_follower/bindings.cpp
@@ -8,7 +8,6 @@
 
 namespace py = pybind11;
 
-
 PYBIND11_MODULE(pyjacobian_follower, m)
 {
   py::class_<JacobianFollower>(m, "JacobianFollower")
@@ -16,7 +15,27 @@ PYBIND11_MODULE(pyjacobian_follower, m)
            py::arg("robot_namespace"),
            py::arg("translation_step_size"),
            py::arg("minimize_rotation"))
-      .def("plan", &JacobianFollower::plan,
+      .def("plan_from_start_state", py::overload_cast<std::string const &,
+               std::vector<std::string> const &,
+               std::vector<Eigen::Vector4d> const &,
+               moveit_msgs::RobotState const &,
+               std::vector<std::vector<Eigen::Vector3d>> const &,
+               double,
+               double>(&JacobianFollower::plan_return_msg),
+           py::arg("group_name"),
+           py::arg("tool_names"),
+           py::arg("preferred_tool_orientations"),
+           py::arg("start_state"),
+           py::arg("grippers"),
+           py::arg("max_velocity_scaling_factor"),
+           py::arg("max_acceleration_scaling_factor")
+      )
+      .def("plan", py::overload_cast<std::string const &,
+               std::vector<std::string> const &,
+               std::vector<Eigen::Vector4d> const &,
+               std::vector<std::vector<Eigen::Vector3d>> const &,
+               double,
+               double>(&JacobianFollower::plan_return_msg),
            py::arg("group_name"),
            py::arg("tool_names"),
            py::arg("preferred_tool_orientations"),
