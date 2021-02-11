aee25d08d7e6634fde4dff7c0a64782ff529752a
Peter Mitrano <pmitrano@armstorm>
2021-02-11 12:15:43 -0500

    deduplicate code

--- arm_robots/src/arm_robots/get_robot.py
+++ arm_robots/src/arm_robots/get_robot.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Type
 
 import rospy
 from arm_robots.hdt_michigan import Val, BaseVal
@@ -6,41 +6,21 @@ from arm_robots.victor import BaseVictor, Victor
 
 
 def get_moveit_robot(robot_name: Optional[str] = None, **kwargs):
+    get_robot(Val, Victor, robot_name, **kwargs)
+
+
+def get_base_robot(robot_name: Optional[str] = None, **kwargs):
+    get_robot(BaseVal, BaseVictor, robot_name, **kwargs)
+
+
+def get_robot(val_type: Type, victor_type: Type, robot_name: Optional[str] = None, **kwargs):
     """
     Get the right robot. It considers first the robot_name argument,
     then checks the ros parameter server,
     then the ROS_NAMESPACE variable.
     Args:
-        robot_name: robot name
-        **kwargs:
-
-    Returns:
-
-    """
-    default = 'victor'
-    if robot_name is None:
-        if rospy.has_param("robot_name"):
-            robot_name = rospy.get_param("robot_name")
-        elif rospy.get_namespace() != "/":
-            robot_name = rospy.get_namespace().strip("/")
-        else:
-            rospy.logwarn(f"using default robot_name{default}")
-            robot_name = default
-
-    if robot_name == 'victor':
-        return Victor('victor', **kwargs)
-    elif robot_name in ['val', 'hdt_michigan']:
-        return Val('hdt_michigan', **kwargs)
-    else:
-        raise NotImplementedError(f"robot with name {robot_name} not implemented")
-
-
-def get_base_robot(robot_name: Optional[str] = None):
-    """
-    Get the right robot. It considers first the robot_name argument,
-    then checks the ros parameter server,
-    then the ROS_NAMESPACE variable.
-    Args:
+        victor_type: the class to instantiate for victor
+        val_type:  the class to instantiate for val
         robot_name: robot name
 
     Returns:
@@ -57,8 +37,8 @@ def get_base_robot(robot_name: Optional[str] = None):
             robot_name = default
 
     if robot_name == 'victor':
-        return BaseVictor(robot_name)
+        return victor_type(robot_name, **kwargs)
     elif robot_name in ['val', 'hdt_michigan']:
-        return BaseVal(robot_name)
+        return val_type(robot_name, **kwargs)
     else:
         raise NotImplementedError(f"robot with name {robot_name} not implemented")
