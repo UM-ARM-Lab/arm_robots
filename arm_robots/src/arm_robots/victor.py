8ea7291ce90fa7b3b83a7a1c9a5a3d0a4510f742
Peter <peter@armnova>
2021-05-04 15:38:38 -0400

    tweak gripper api for victor

--- arm_robots/src/arm_robots/victor.py
+++ arm_robots/src/arm_robots/victor.py
@@ -2,13 +2,21 @@
 import collections
 from typing import List, Dict, Tuple, Sequence
 
-import numpy as np
-from colorama import Fore
-from scipy import signal
-
 import moveit_commander
+import numpy as np
 import rospy
 from actionlib import SimpleActionClient
+from colorama import Fore
+from control_msgs.msg import FollowJointTrajectoryFeedback, FollowJointTrajectoryGoal, FollowJointTrajectoryResult
+from moveit_msgs.msg import DisplayRobotState
+from scipy import signal
+from std_msgs.msg import String, Float32
+from trajectory_msgs.msg import JointTrajectoryPoint
+from victor_hardware_interface_msgs.msg import ControlMode, MotionStatus, MotionCommand, Robotiq3FingerCommand, \
+    Robotiq3FingerStatus
+from victor_hardware_interface_msgs.srv import SetControlMode, GetControlMode, GetControlModeRequest, \
+    GetControlModeResponse, SetControlModeResponse
+
 from arc_utilities.conversions import convert_to_pose_msg, normalize_quaternion, convert_to_positions
 from arc_utilities.listener import Listener
 from arm_robots.base_robot import DualArmRobot
@@ -19,16 +27,8 @@ from arm_robots.config.victor_config import NAMED_POSITIONS, default_robotiq_com
     KUKA_FULL_SPEED_PATH_JOINT_IMPEDANCE_TOLERANCE, KUKA_GOAL_JOINT_IMPEDANCE_TOLERANCE
 from arm_robots.robot import MoveitEnabledRobot
 from arm_robots.robot_utils import make_joint_tolerance
-from control_msgs.msg import FollowJointTrajectoryFeedback, FollowJointTrajectoryGoal, FollowJointTrajectoryResult
-from moveit_msgs.msg import DisplayRobotState
-from std_msgs.msg import String, Float32
-from trajectory_msgs.msg import JointTrajectoryPoint
 from victor_hardware_interface.victor_utils import get_control_mode_params, list_to_jvq, jvq_to_list, \
-    default_gripper_command, gripper_status_to_list
-from victor_hardware_interface_msgs.msg import ControlMode, MotionStatus, MotionCommand, Robotiq3FingerCommand, \
-    Robotiq3FingerStatus
-from victor_hardware_interface_msgs.srv import SetControlMode, GetControlMode, GetControlModeRequest, \
-    GetControlModeResponse, SetControlModeResponse
+    default_gripper_command, gripper_status_to_list, is_gripper_closed
 
 
 def delegate_to_arms(positions: List, joint_names: Sequence[str]) -> Tuple[Dict[str, List], bool, str]:
@@ -65,10 +65,10 @@ def delegate_to_arms(positions: List, joint_names: Sequence[str]) -> Tuple[Dict[
         return [joint_position_of[name] for name in joint_ordering]
 
     positions_by_interface = {
-        'right_arm'    : fill_using(RIGHT_ARM_JOINT_NAMES),
-        'left_arm'     : fill_using(LEFT_ARM_JOINT_NAMES),
+        'right_arm':     fill_using(RIGHT_ARM_JOINT_NAMES),
+        'left_arm':      fill_using(LEFT_ARM_JOINT_NAMES),
         'right_gripper': fill_using(RIGHT_GRIPPER_JOINT_NAMES),
-        'left_gripper' : fill_using(LEFT_GRIPPER_JOINT_NAMES),
+        'left_gripper':  fill_using(LEFT_GRIPPER_JOINT_NAMES),
     }
     # set equality ignores order
 
@@ -163,7 +163,7 @@ class BaseVictor(DualArmRobot):
         command_pub.publish(cmd)
 
     def get_gripper_statuses(self):
-        return {'left' : self.get_left_gripper_status(),
+        return {'left':  self.get_left_gripper_status(),
                 'right': self.get_right_gripper_status()}
 
     def get_right_gripper_status(self) -> Robotiq3FingerStatus:
@@ -173,22 +173,22 @@ class BaseVictor(DualArmRobot):
         return self.left_gripper_status_listener.get()
 
     def is_left_gripper_closed(self):
-        status = self.get_left_gripper_status()
-        return self.is_gripper_closed(status)
+        return self.is_gripper_closed('left')
 
     def is_right_gripper_closed(self):
-        status = self.get_right_gripper_status()
-        return self.is_gripper_closed(status)
+        return self.is_gripper_closed('right')
 
-    def is_gripper_closed(self, status: Robotiq3FingerStatus):
-        # [0.5, 0.4, 0.4, 0.8]
-        finger_a_closed = status.finger_a_status.position > 0.5 - 1e-2
-        finger_b_closed = status.finger_b_status.position > 0.5 - 1e-2
-        finger_c_closed = status.finger_c_status.position > 0.5 - 1e-2
-        return finger_a_closed and finger_b_closed and finger_c_closed
+    def is_gripper_closed(self, gripper: str):
+        if gripper == 'left':
+            status = self.get_left_gripper_status()
+        elif gripper == 'right':
+            status = self.get_right_gripper_status()
+        else:
+            raise NotImplementedError(f"invalid gripper {gripper}")
+        return is_gripper_closed(status)
 
     def get_arms_statuses(self):
-        return {'left' : self.get_left_arm_status(),
+        return {'left':  self.get_left_arm_status(),
                 'right': self.get_right_arm_status()}
 
     def get_right_arm_status(self) -> MotionStatus:
@@ -286,7 +286,7 @@ class BaseVictor(DualArmRobot):
         desired_right_pose.position.z += delta_positions['right'].z
 
         poses = {
-            'left' : desired_left_pose,
+            'left':  desired_left_pose,
             'right': desired_right_pose,
         }
         self.send_cartesian_command(poses)
