46688205235c5519343bffabd55476a372027bd6
Peter <peter@armnova>
2021-05-04 14:40:05 -0400

    address review

--- jacobian_follower/src/jacobian_follower/bindings.cpp
+++ jacobian_follower/src/jacobian_follower/bindings.cpp
@@ -13,26 +13,25 @@ PYBIND11_MODULE(pyjacobian_follower, m) {
       .def(py::init<std::string, double, bool, bool, bool>(), py::arg("robot_namespace"),
            py::arg("translation_step_size"), py::arg("minimize_rotation"), py::arg("collision_check"),
            py::arg("visualize"))
-      .def("plan_from_scene_and_state",
+      .def("plan",
            py::overload_cast<std::string const &, std::vector<std::string> const &,
                              std::vector<Eigen::Vector4d> const &, moveit_msgs::RobotState const &,
-                             moveit_msgs::PlanningScene const &, std::vector<PointSequence> const &, double, double>(
-               &JacobianFollower::plan_from_scene_and_state),
+                             moveit_msgs::PlanningScene const &, std::vector<PointSequence> const &, double, double>(&JacobianFollower::plan),
            py::arg("group_name"), py::arg("tool_names"), py::arg("preferred_tool_orientations"), py::arg("start_state"),
            py::arg("scene"),
            py::arg("grippers"),  // TODO: rename 'grippers' to 'tool_names'
            py::arg("max_velocity_scaling_factor"), py::arg("max_acceleration_scaling_factor"))
       .def(
-          "plan_from_state",
+          "plan",
           py::overload_cast<std::string const &, std::vector<std::string> const &, std::vector<Eigen::Vector4d> const &,
                             moveit_msgs::RobotState const &, std::vector<PointSequence> const &, double, double>(
-              &JacobianFollower::plan_from_state),
+              &JacobianFollower::plan),
           py::arg("group_name"), py::arg("tool_names"), py::arg("preferred_tool_orientations"), py::arg("start_state"),
           py::arg("grippers"), py::arg("max_velocity_scaling_factor"), py::arg("max_acceleration_scaling_factor"))
       .def(
           "plan",
           py::overload_cast<std::string const &, std::vector<std::string> const &, std::vector<Eigen::Vector4d> const &,
-                            std::vector<PointSequence> const &, double, double>(&JacobianFollower::plan_return_msg),
+                            std::vector<PointSequence> const &, double, double>(&JacobianFollower::plan),
           py::arg("group_name"), py::arg("tool_names"), py::arg("preferred_tool_orientations"), py::arg("grippers"),
           py::arg("max_velocity_scaling_factor"), py::arg("max_acceleration_scaling_factor"))
       .def("compute_IK_solutions", &JacobianFollower::compute_IK_solutions, py::arg("pose"),
