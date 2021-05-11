2b081fb56cfc1d50259e94014c3b0bcf78cdc28b
Mark Van der Merwe <markvdm@umich.edu>
2021-05-11 19:59:28 -0400

    Messing around with impedance in med_motion.

--- arm_robots/scripts/med_motion.py
+++ arm_robots/scripts/med_motion.py
@@ -6,15 +6,16 @@ import pdb
 import moveit_commander
 from arc_utilities.conversions import convert_to_pose_msg
 from victor_hardware_interface_msgs.msg import ControlMode
+from victor_hardware_interface.victor_utils import Stiffness
 
 def main():
     rospy.init_node('med_motion')
 
     med = Med(manual_execute=True)
     med.connect()
-    med.set_control_mode(ControlMode.JOINT_POSITION, vel=0.15)
+    med.set_control_mode(ControlMode.JOINT_POSITION, vel=0.1)
     # med.set_grasping_force(40.0)
-    med.open_gripper()
+    # med.open_gripper()
 
     # Add table plane.
     # scene = moveit_commander.PlanningSceneInterface(ns="victor")
@@ -22,29 +23,33 @@ def main():
 
     # Pick and place "demo"
     med.plan_to_joint_config(med.arm_group, [0,0,0,0,0,0,0])
-
-    while True:
-        _, result, _ = med.plan_to_pose(med.arm_group, med.wrist, [0.6, 0.12, 0.45, 0.0, np.pi, 0.0], frame_id='med_base')
-        if result.error_code < 0:
-            replan = input('Replan? [y/n]')
-            if replan != 'y':
-                exit()
-        else:
-            break
-
-    med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, 0.12, 0.35])
-    #med.grasp(15.0)
-    med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, 0.12, 0.45])
-    med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, -0.12, 0.45])
-    med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, -0.12, 0.365])
-    #med.release()
-    med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, -0.12, 0.45])
-    med.plan_to_joint_config(med.arm_group, [0,0,0,0,0,0,0])
+    exit()
+    #
+    # while True:
+    #     _, result, _ = med.plan_to_pose(med.arm_group, med.wrist, [0.6, 0.12, 0.45, 0.0, np.pi, 0.0], frame_id='med_base')
+    #     if result.error_code < 0:
+    #         replan = input('Replan? [y/n]')
+    #         if replan != 'y':
+    #             exit()
+    #     else:
+    #         break
+    #
+    # med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, 0.12, 0.35])
+    # #med.grasp(15.0)
+    # med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, 0.12, 0.45])
+    # med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, -0.12, 0.45])
+    # med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, -0.12, 0.365])
+    # #med.release()
+    # med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, -0.12, 0.45])
+    # med.plan_to_joint_config(med.arm_group, [0,0,0,0,0,0,0])
 
     # med.grasp(width=10.0)
     # Plan to joint config.
-    # med.plan_to_joint_config(med.arm_group, [0,1.0,0,1.0,0,1.0,0])
-    # med.plan_to_joint_config(med.arm_group, [0,0,0,0,0,0,0])
+    med.plan_to_joint_config(med.arm_group, [0.0,1.0,0.0,1.0,0,1.0,0])
+    input('Start impedance.')
+    med.set_control_mode(ControlMode.JOINT_IMPEDANCE, vel=0.1, stiffness=Stiffness.SOFT)
+    input('Go home.')
+    med.plan_to_joint_config(med.arm_group, [0,0,0,0,0,0,0])
 
     # Plan to position (x,y,z) relative to robot base.
     # med.plan_to_position(med.arm_group, med.wrist, [0.6, 0.0, 0.6])
