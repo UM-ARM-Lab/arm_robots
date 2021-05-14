6d6e03f540b588985200c56b9940f6966a0af4d1
Mark Van der Merwe <markvdm@umich.edu>
2021-05-13 21:39:35 -0400

    Fix up med launch and trajectory following for real world test

--- arm_robots/scripts/med_motion.py
+++ arm_robots/scripts/med_motion.py
@@ -5,33 +5,43 @@ import numpy as np
 import pdb
 import moveit_commander
 from arc_utilities.conversions import convert_to_pose_msg
-
+from victor_hardware_interface_msgs.msg import ControlMode
+from victor_hardware_interface.victor_utils import Stiffness
 
 def main():
     rospy.init_node('med_motion')
 
     med = Med(display_goals=False)
     med.connect()
+    med.set_control_mode(ControlMode.JOINT_POSITION, vel=0.3)
     # med.set_grasping_force(40.0)
     # med.open_gripper()
 
-    med.plan_to_joint_config(med.arm_group, [0, 1.0, 0, -1.0, 0, 1.0, 0])
-    med.plan_to_pose(med.arm_group, med.wrist, [0.6, 0.12, 0.45, 0.0, np.pi, 0.0], frame_id='med_base')
-    med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, -0.12, 0.35])
-    med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, -0.12, 0.45])
-    med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, 0.12, 0.45])
-    med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, 0.12, 0.365])
-    med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, 0.12, 0.45])
-    med.plan_to_joint_config(med.arm_group, [0, 0, 0, 0, 0, 0, 0])
-
+    med.plan_to_joint_config(med.arm_group, [0.0,1.0,0.0,1.0,0,1.0,0])
+    # input('Start impedance.')
+    # med.set_control_mode(ControlMode.JOINT_IMPEDANCE, vel=0.1, stiffness=Stiffness.SOFT)
+    # input('Go home.')
+    # med.set_control_mode(ControlMode.JOINT_POSITION, vel=0.1)
+    # med.plan_to_joint_config(med.arm_group, [0, 0, 0, 0, 0, 0, 0])
     exit()
+
+    # med.plan_to_joint_config(med.arm_group, [0, 1.0, 0, -1.0, 0, 1.0, 0])
+    # med.plan_to_pose(med.arm_group, med.wrist, [0.6, 0.12, 0.45, 0.0, np.pi, 0.0], frame_id='med_base')
+    # med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, -0.12, 0.35])
+    # med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, -0.12, 0.45])
+    # med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, 0.12, 0.45])
+    # med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, 0.12, 0.365])
+    # med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, 0.12, 0.45])
+    # med.plan_to_joint_config(med.arm_group, [0, 0, 0, 0, 0, 0, 0])
+
+    # exit()
 
     # Add table plane.
     # scene = moveit_commander.PlanningSceneInterface(ns="victor")
     # scene.add_plane('table_plane', convert_to_pose_msg([0,0,0,0,0,0]))
 
     # Pick and place "demo"
-    med.plan_to_joint_config(med.arm_group, [0, 0, 0, 0, 0, 0, 0])
+    # med.plan_to_joint_config(med.arm_group, [0, 0, 0, 0, 0, 0, 0])
 
     # while True:
     #     _, result, _ = med.plan_to_pose(med.arm_group, med.wrist, [0.6, 0.12, 0.45, 0.0, np.pi, 0.0],
@@ -43,17 +53,17 @@ def main():
     #     else:
     #         break
 
-    med.plan_to_joint_config(med.arm_group, [0, 0, 0, 0, 0, 0, 0])
-    exit()
+    # med.plan_to_joint_config(med.arm_group, [0, 0, 0, 0, 0, 0, 0])
+    # exit()
 
-    med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, -0.12, 0.35])
-    med.grasp(15.0)
-    med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, -0.12, 0.45])
-    med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, 0.12, 0.45])
-    med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, 0.12, 0.365])
-    med.release()
-    med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, 0.12, 0.45])
-    med.plan_to_joint_config(med.arm_group, [0, 0, 0, 0, 0, 0, 0])
+    # med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, -0.12, 0.35])
+    # med.grasp(15.0)
+    # med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, -0.12, 0.45])
+    # med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, 0.12, 0.45])
+    # med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, 0.12, 0.365])
+    # med.release()
+    # med.plan_to_position_cartesian(med.arm_group, med.wrist, target_position=[0.6, 0.12, 0.45])
+    # med.plan_to_joint_config(med.arm_group, [0, 0, 0, 0, 0, 0, 0])
 
     # med.grasp(width=10.0)
     # Plan to joint config.
