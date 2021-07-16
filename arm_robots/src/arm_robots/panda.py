7b0daca4fe986cb486c1aa94fceff971cc5dc21e
Mark Van der Merwe <markvdm@umich.edu>
2021-07-16 14:11:06 -0400

    Boilerplate for setting up Panda abstraction.

---
+++ arm_robots/src/arm_robots/panda.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+from typing import List, Tuple
+
+from arm_robots.robot import MoveitEnabledRobot
+from trajectory_msgs.msg import JointTrajectoryPoint
+
+
+class Panda(MoveitEnabledRobot):
+
+    def __init__(self, robot_namespace: str = 'combined_panda', force_trigger: float = -0.0, **kwargs):
+        MoveitEnabledRobot.__init__(self,
+                                    robot_namespace=robot_namespace,
+                                    arms_controller_name='effort_joint_trajectory_controller',  # TODO: Double check me!
+                                    force_trigger=force_trigger,
+                                    **kwargs)
+
+    def send_joint_command(self, joint_names: List[str], trajectory_point: JointTrajectoryPoint) -> Tuple[bool, str]:
+        # TODO: Fill in to send set point to controller.
+        pass
+
+    # TODO: Add gripper helpers.
+
+    # TODO: Add control mode setter/getter.
