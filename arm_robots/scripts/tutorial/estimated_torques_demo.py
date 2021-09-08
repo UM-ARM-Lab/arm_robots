0ca9e92281e1cf244e878e713f42b39b3b5e0f5c
administrator <administrator@val>
2021-09-08 16:02:54 -0400

    Updates to robot_description and namespacing
    add with_husky arg
    don't make world a child of robot_root when running in the real world
    fix gazebo plugin robot name
    move robot_root frame to URDF
    set default robot_description for BaseVal to be namespaced
    update val_basic_motion
    current calibration/limiting

--- arm_robots/scripts/tutorial/estimated_torques_demo.py
+++ arm_robots/scripts/tutorial/estimated_torques_demo.py
@@ -5,6 +5,8 @@ import sys
 import rospy
 from arc_utilities import ros_init
 from arm_robots.get_robot import get_moveit_robot
+from arm_robots.hdt_michigan import estimated_torques
+from arm_robots.robot_utils import robot_state_from_joint_state_and_joint_names
 from geometry_msgs.msg import Wrench
 from moveit_msgs.msg import RobotState
 from sensor_msgs.msg import JointState
@@ -29,17 +31,6 @@ torque_viz_scales = {
 }
 
 
-def robot_state_from_joint_state_and_joint_names(joint_names, joint_state):
-    robot_state = RobotState()
-    for name in joint_names:
-        i = joint_state.name.index(name)
-        p = joint_state.position[i]
-        robot_state.joint_state.name.append(name)
-        robot_state.joint_state.position.append(p)
-        robot_state.joint_state.velocity.append(0)
-    return robot_state
-
-
 @ros_init.with_ros("estimated_torques_demo")
 def main():
     """
@@ -53,45 +44,8 @@ def main():
 
     robot = get_moveit_robot(args.robot_namespace)
 
-    print(f"Using robot {robot}")
-
-    group_name1 = "left_side"
-    group_name2 = "right_side"
-    n_links = 14
-    wrenches = [Wrench()] * n_links
-
     def _joint_state_cb(joint_state: JointState):
-        joint_names1 = robot.robot_commander.get_active_joint_names(group_name1)
-        robot_state1 = robot_state_from_joint_state_and_joint_names(joint_names1, joint_state)
-        torques1 = robot.estimated_torques(robot_state1, group_name1, wrenches)
-
-        joint_names2 = robot.robot_commander.get_active_joint_names(group_name2)
-        robot_state2 = robot_state_from_joint_state_and_joint_names(joint_names2, joint_state)
-        torques2 = robot.estimated_torques(robot_state2, group_name2, wrenches)
-
-        if torques1 is None or torques2 is None:
-            return
-
-        robot_state = RobotState()
-        names = sorted(set(robot_state1.joint_state.name + robot_state2.joint_state.name))
-        for name in names:
-            p = None
-            e = None
-            try:
-                i = robot_state1.joint_state.name.index(name)
-                p = robot_state1.joint_state.position[i]
-                e = torques1[i]
-            except ValueError:
-                pass
-            try:
-                i = robot_state2.joint_state.name.index(name)
-                p = robot_state2.joint_state.position[i]
-                e = torques2[i]
-            except ValueError:
-                pass
-            robot_state.joint_state.name.append(name)
-            robot_state.joint_state.position.append(p)
-            robot_state.joint_state.effort.append(e)
+        robot_state = estimated_torques(joint_state, robot)
 
         for name, scale_factor in torque_viz_scales.items():
             i = robot_state.joint_state.name.index(name)
--- arm_robots/scripts/tutorial/val_basic_motion.py
+++ arm_robots/scripts/tutorial/val_basic_motion.py
@@ -37,15 +37,15 @@ def main():
 
     # Plan to pose
     myinput("Plan to pose 1?")
-    val.plan_to_pose(val.right_arm_group, val.right_tool_name, [0.3, 0.8, 0.5, -np.pi / 2, np.pi / 2, 0])
+    val.plan_to_pose(val.right_arm_group, val.right_tool_name, [0.8, -0.3, 0.35, 0, np.pi, -np.pi / 2])
 
     # Or you can use a geometry msgs Pose
     myinput("Plan to pose 2?")
     pose = Pose()
-    pose.position.x = 0.2
-    pose.position.y = 0.5
-    pose.position.z = 0.1
-    q = quaternion_from_euler(-np.pi, 0, 0)
+    pose.position.x = 0.8
+    pose.position.y = -0.2
+    pose.position.z = 0.4
+    q = quaternion_from_euler(0, np.pi, -np.pi / 4)
     pose.orientation.x = q[0]
     pose.orientation.y = q[1]
     pose.orientation.z = q[2]
@@ -54,11 +54,11 @@ def main():
 
     # or with cartesian planning
     myinput("cartesian motion to pose 3?")
-    val.plan_to_position_cartesian(val.right_arm_group, val.right_tool_name, [0.4, 0.3, 0.1], step_size=0.01)
+    val.plan_to_position_cartesian(val.right_arm_group, val.right_tool_name, [0.8, -0.3, 0.4], step_size=0.01)
 
     myinput("follow jacobian to pose 4?")
     val.store_current_tool_orientations([val.right_tool_name])
-    val.follow_jacobian_to_position('both_arms', [val.right_tool_name], [[[0.3, 0.5, 0.3]]], vel_scaling=1.0)
+    val.follow_jacobian_to_position('both_arms', [val.right_tool_name], [[[0.8, -0.2, 0.4]]], vel_scaling=1.0)
 
     val.disconnect()
 
