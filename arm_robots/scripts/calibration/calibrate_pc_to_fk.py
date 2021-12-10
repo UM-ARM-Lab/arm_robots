7bed14d9037b3317f040a9a24a1470d27102bfd2
Peter <peter@armnova>
2021-12-10 15:30:46 -0500

    calibration of fk to pointcloud

--- arm_robots/scripts/calibration/calibrate_pc_to_fk.py
+++ arm_robots/scripts/calibration/calibrate_pc_to_fk.py
@@ -1,9 +1,11 @@
+import argparse
 import copy
+
+import numpy as np
 import open3d as o3d
 import transformations
 
 from arc_utilities import ros_init
-import numpy as np
 from arc_utilities.tf2wrapper import TF2Wrapper
 from arm_robots.basic_3d_pose_interactive_marker import Basic3DPoseInteractiveMarker
 from arm_robots.hdt_michigan import Val
@@ -24,12 +26,16 @@ def draw_registration_result(source, target, transformation):
 
 @ros_init.with_ros("calibrate_pc_to_fk")
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--visualize', action='store_true')
+
+    args = parser.parse_args()
+
     val = Val(raise_on_failure=True)
     val._max_velocity_scale_factor = 1.0
     val.connect()
 
     frame_a = 'mocap_world'
-    frame_b = 'mocap_val_root_val_root'
     radius = 0.005
 
     def make_left_marker(scale: float):
@@ -67,7 +73,8 @@ def main():
 
     pc_points = []
     fk_points = []
-    for i in range(6):
+    for i in range(10):
+        # Move to a random config
         joint_config_ = val.get_move_group_commander('both_arms').get_random_joint_values()
         joint_config = (np.array(joint_config_) * 0.05).tolist()
         val.plan_to_joint_config('both_arms', joint_config)
@@ -77,10 +84,12 @@ def main():
         right_fk_transform = tf.get_transform('mocap_val_root_val_root', 'right_tool')
         right_fk_point = right_fk_transform[:3, -1]
 
+        # initialize with FK to make it easier/faster
         left_tool_im.set_pose(Pose(point=Point(*left_fk_point)))
         right_tool_im.set_pose(Pose(point=Point(*right_fk_point)))
 
-        input("move marker...")
+        input("move markers, then press enter to continue...")
+
         left_pose_frame_a = left_tool_im.get_pose()
         left_pc_point = numpify(left_pose_frame_a.position)
         right_pose_frame_a = right_tool_im.get_pose()
@@ -101,20 +110,18 @@ def main():
     fk_points_pcd.points = o3d.utility.Vector3dVector(fk_points)
 
     threshold = 0.1
-    init = transformations.compose_matrix(angles=np.deg2rad([-0.262, 0.810, 50.48]), translate=[1.197, -0.938, -1.198])
-    evaluation = o3d.pipelines.registration.evaluate_registration(fk_points_pcd, pc_points_pcd, threshold, init)
-    draw_registration_result(fk_points_pcd, pc_points_pcd, init)
-    print(evaluation)
+    # init was copied from the Vicon software
+    init = transformations.compose_matrix(angles=np.deg2rad([-0.226, -1.82, 49.329]), translate=[1.219, -0.932, -1.206])
 
     reg_p2p = o3d.pipelines.registration.registration_icp(fk_points_pcd, pc_points_pcd, threshold, init,
                                                           o3d.pipelines.registration.TransformationEstimationPointToPoint(),
                                                           o3d.pipelines.registration.ICPConvergenceCriteria(
                                                               max_iteration=2000))
-    evaluation = o3d.pipelines.registration.evaluate_registration(fk_points_pcd, pc_points_pcd, threshold, reg_p2p.transformation)
-    print(reg_p2p.transformation)
-    print(evaluation)
-    draw_registration_result(fk_points_pcd, pc_points_pcd, reg_p2p.transformation)
 
+    if args.visualize:
+        draw_registration_result(fk_points_pcd, pc_points_pcd, reg_p2p.transformation)
+
+    print("Results:")
     print(np.rad2deg(transformations.euler_from_matrix(reg_p2p.transformation)))
     print(transformations.translation_from_matrix(reg_p2p.transformation) * 1000)
 
