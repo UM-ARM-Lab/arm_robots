01e39d82d1012beca63f8c192c712074e9f7b7a1
Peter <peter@thinkpad_t15>
2024-02-09 00:01:30 -0500

    start converting

--- arm_robots/scripts/utility/move_to_impedance_switch.py
+++ arm_robots/scripts/utility/move_to_impedance_switch.py
@@ -2,32 +2,43 @@
 
 """
 This moves Victor's arms to a configuration where impedance validations tends to work well.
-if you want to actually change the control mode, not just move to the position, set "_actually_switch:=true"
+if you want to actually change the control mode, not just move to the position, add the argument --actually-switch
 """
-import colorama
-from colorama import Fore
 
-import rospy
+import argparse
+
+import rclpy
+from rclpy.node import Node
+
 from arm_robots.victor import Victor
+from arm_robots.victor_planning import move_to_group_state
+from victor_hardware_interfaces.msg import ControlMode
+
+
+class MoveToImpedanceSwitch(Node):
+
+    def __init__(self, actually_switch=False):
+        super().__init__('move_to_impedance_switch')
+        victor = Victor(self)
+
+        move_to_group_state(victor, 'both_arms', 'impedance_switch')
+
+        if actually_switch:
+            victor.set_control_mode(ControlMode.JOINT_IMPEDANCE)
+
+
+def main():
+    rclpy.init()
+
+    parser = argparse.ArgumentParser(description='Move Victor to a configuration where impedance control works well')
+    parser.add_argument('--actually-switch', action='store_true', help='Actually switch to impedance control')
+
+    args = parser.parse_args()
+
+    node = MoveToImpedanceSwitch(args.actually_switch)
+
+    rclpy.spin(node)
+
 
 if __name__ == "__main__":
-    colorama.init(autoreset=True)
-    rospy.init_node("move_to_impedance_switch")
-    rospy.logwarn("Make sure you set up a conservative set of obstacles in gazebo")
-    rospy.logwarn("View the planning scene in RViz to see what the planner is aware of")
-    k = input("Did you set up a conservative set of obstacles in gazebo? [y/N]")
-    if k == 'Y' or k == 'y':
-        rospy.loginfo(Fore.CYAN + "you better not be lying...")
-
-        victor = Victor(robot_namespace='victor')
-        victor.connect()
-
-        actually_switch = rospy.get_param("~actually_switch", False)
-        if actually_switch:
-            rospy.loginfo("switching to impedance mode")
-
-        victor.move_to_impedance_switch(actually_switch=actually_switch)
-
-        rospy.loginfo("Done")
-    else:
-        rospy.loginfo("Answered 'no', aborting")
+    main()
--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = 'arm_robots'
+
+pkg_files_to_install = ['package.xml', ] + glob("rviz/*.rviz") + glob("launch/*.launch.*") 
 
 setup(
     name=package_name,
@@ -8,19 +11,18 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('rviz', ['rviz'])
-        ('launch', ['launch'])
+        ('share/' + package_name, pkg_files_to_install),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='peter',
-    maintainer_email='peter@thinkpad_t15',
+    maintainer_email='pmitrano@umich.edu',
     description='simple python API for commanding some of our robots',
     license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'move_to_impedance_switch = arm_robots.scripts.utility.move_to_impedance_switch:main',
         ],
     },
 )
