1a68e436a30810dfa86ac57feadf99e9a2b40367
Peter <peter@thinkpad_t15>
2024-02-08 22:52:25 -0500

    tweak setup.py

--- setup.py
+++ setup.py
@@ -7,15 +7,16 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('rviz', ['rviz'])
+        ('launch', ['launch'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='peter',
     maintainer_email='peter@thinkpad_t15',
-    description='TODO: Package description',
+    description='simple python API for commanding some of our robots',
     license='MIT',
     tests_require=['pytest'],
     entry_points={
