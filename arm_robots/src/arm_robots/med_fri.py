22910278cd3c8eb14cb5fb28cfa0ab5ac5249053
dangthanhan507 <dangthanhan507@gmail.com>
2024-12-30 21:36:21 -0500

    Added ros and lcm sub/pub.

--- arm_robots/src/arm_robots/med_fri.py
+++ arm_robots/src/arm_robots/med_fri.py
@@ -1,92 +1,70 @@
 #! /usr/bin/env python
 import rospy
-import rospkg
-from manipulation.station import MakeHardwareStation, MakeHardwareStationInterface, load_scenario
-import os
-from pydrake.all import (
-    LcmSubscriberSystem,
-    IiwaDriver,
-    ApplyLcmBusConfig,
-    DiagramBuilder,
-    DrakeLcmInterface,
-    Subscriber,
-    DrakeLcm
-)
 from drake import lcmt_iiwa_status
-'''
-use drake to get lcm
-'''
-DUAL_SCENARIO_STRING = '''
-Demo:
-  directives:
-      # Add iiwa_medusa
-      - add_model:
-          name: iiwa_medusa
-          file: package://med_description/urdf/med_fri.urdf
-          default_joint_positions:
-              iiwa_joint_1: [0]
-              iiwa_joint_2: [0]
-              iiwa_joint_3: [0]
-              iiwa_joint_4: [0]
-              iiwa_joint_5: [0]
-              iiwa_joint_6: [0]
-              iiwa_joint_7: [0]
-      # Add iiwa_thanos
-      - add_model:
-          name: iiwa_thanos
-          file: package://med_description/urdf/med_fri.urdf
-          default_joint_positions:
-              iiwa_joint_1: [0]
-              iiwa_joint_2: [0]
-              iiwa_joint_3: [0]
-              iiwa_joint_4: [0]
-              iiwa_joint_5: [0]
-              iiwa_joint_6: [0]
-              iiwa_joint_7: [0]
-      - add_frame:
-          name: iiwa_medusa_origin
-          X_PF:
-              base_frame: world
-              rotation: !Rpy { deg: [0.0, 0.0, 0.0]}
-              translation: [0, 1.2192, 0.0]
-      - add_weld:
-          parent: iiwa_medusa_origin
-          child: iiwa_medusa::base
-      - add_weld:
-          parent: world
-          child: iiwa_thanos::base
-
-  lcm_buses:
-    medusa_lcm:
-      channel_suffix: _MEDUSA
-    thanos_lcm:
-      channel_suffix: _THANOS
-  model_drivers:
-    iiwa_medusa: !IiwaDriver
-      control_mode: position_only
-      lcm_bus: medusa_lcm
-    iiwa_thanos: !IiwaDriver
-      control_mode: position_only
-      lcm_bus: thanos_lcm
-'''
-
+from victor_hardware_interface_msgs.msg import MotionStatus
+from sensor_msgs.msg import JointState
 import lcm
 
+class MedFRILCM:
+    def __init__(self):
+        self.lcm = lcm.LCM()
+        self.medusa_sub = self.lcm.subscribe('IIWA_STATUS_MEDUSA', lambda channel, data: self.medusa_msg_handler(channel, data))
+        self.thanos_sub = self.lcm.subscribe('IIWA_STATUS_THANOS', lambda channel, data: self.thanos_msg_handler(channel, data))
+        
+        self.thanos_status_pub = rospy.Publisher('thanos_med/motion_status', MotionStatus, queue_size=1)
+        self.thanos_joints_pub = rospy.Publisher('thanos_med/joint_states', JointState, queue_size=1)
+        
+        self.medusa_status_pub = rospy.Publisher('medusa_med/motion_status', MotionStatus, queue_size=1)        
+        self.medusa_joints_pub = rospy.Publisher('medusa_med/joint_states', JointState, queue_size=1)
+        
+    def thanos_msg_handler(self, channel, data):
+        thanos_fri_msg = lcmt_iiwa_status.decode(data)
+        
+        thanos_status = MotionStatus()
+        thanos_status.header.stamp = rospy.Time.now()
+        thanos_status.commanded_joint_position  = thanos_fri_msg.joint_position_commanded
+        
+        thanos_status.measured_joint_position   = thanos_fri_msg.joint_position_measured
+        thanos_status.measured_joint_velocity   = thanos_fri_msg.joint_velocity_estimated
+        thanos_status.measured_joint_torque     = thanos_fri_msg.joint_torque_measured
+        thanos_status.estimated_external_torque = thanos_fri_msg.joint_torque_external
+        self.thanos_status_pub.publish(self.thanos_status)
+        
+        thanos_joint_state = JointState()
+        thanos_joint_state.header.stamp = rospy.Time.now()
+        thanos_joint_state.name = ['iiwa_joint_1', 'iiwa_joint_2', 'iiwa_joint_3', 'iiwa_joint_4', 'iiwa_joint_5', 'iiwa_joint_6', 'iiwa_joint_7']
+        thanos_joint_state.position = thanos_fri_msg.joint_position_measured
+        thanos_joint_state.velocity = thanos_fri_msg.joint_velocity_estimated
+        thanos_joint_state.effort = thanos_fri_msg.joint_torque_measured
+        self.thanos_joints_pub.publish(thanos_joint_state)
+        
+    def medusa_msg_handler(self, channel, data):
+        medusa_fri_msg = lcmt_iiwa_status.decode(data)
+        
+        medusa_status = MotionStatus()
+        medusa_status.header.stamp = rospy.Time.now()
+        medusa_status.commanded_joint_position  = medusa_fri_msg.joint_position_commanded
+        
+        medusa_status.measured_joint_position   = medusa_fri_msg.joint_position_measured
+        medusa_status.measured_joint_velocity   = medusa_fri_msg.joint_velocity_estimated
+        medusa_status.measured_joint_torque     = medusa_fri_msg.joint_torque_measured
+        medusa_status.estimated_external_torque = medusa_fri_msg.joint_torque_external
+        self.medusa_status_pub.publish(medusa_status)
+        
+        medusa_joint_state = JointState()
+        medusa_joint_state.header.stamp = rospy.Time.now()
+        medusa_joint_state.name = ['iiwa_joint_1', 'iiwa_joint_2', 'iiwa_joint_3', 'iiwa_joint_4', 'iiwa_joint_5', 'iiwa_joint_6', 'iiwa_joint_7']
+        medusa_joint_state.position = medusa_fri_msg.joint_position_measured
+        medusa_joint_state.velocity = medusa_fri_msg.joint_velocity_estimated
+        medusa_joint_state.effort = medusa_fri_msg.joint_torque_measured
+        self.medusa_joints_pub.publish(medusa_joint_state)
+        
+    def handle(self):
+        self.lcm.handle()
 if __name__ == '__main__':
     rospy.init_node('med_fri')
-    rospack = rospkg.RosPack()
-    scenario = load_scenario(data=DUAL_SCENARIO_STRING, scenario_name='Demo')
     
-    # builder = DiagramBuilder()
-    # # real_station = MakeHardwareStationInterface(scenario, package_xmls=[os.path.join(rospack.get_path('med_description'), 'package.xml')])
-    
-    def msg_handler(channel, data):
-        msg = lcmt_iiwa_status.decode(data)
-        print(msg.joint_position_commanded)
-    
-    lcm = lcm.LCM()
-    sub = lcm.subscribe('IIWA_STATUS_MEDUSA', lambda channel, data: msg_handler(channel, data))
+    fri_lcm = MedFRILCM()
     
     while not rospy.is_shutdown():
-        lcm.handle()
-    pass
+        fri_lcm.handle()
