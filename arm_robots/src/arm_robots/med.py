3c623486cb1879475a5e100d77b64fba13b7fab8
mr-mikmik <aglaner@hotmail.com>
2025-02-20 15:16:41 -0500

    adding FakeWSG50 on med.py

--- arm_robots/src/arm_robots/med.py
+++ arm_robots/src/arm_robots/med.py
@@ -19,6 +19,7 @@ from victor_hardware_interface.victor_utils import get_control_mode_params, list
 from victor_hardware_interface_msgs.msg import ControlMode, MotionStatus, MotionCommand
 from victor_hardware_interface_msgs.srv import SetControlMode, GetControlMode, GetControlModeRequest, \
     GetControlModeResponse, SetControlModeResponse
+from wsg_50_utils.fake_wsg_50_gripper import FakeWSG50Gripper
 from wsg_50_utils.wsg_50_gripper import WSG50Gripper
 
 
@@ -161,7 +162,7 @@ class BaseMed(BaseRobot):
 
 class Med(BaseMed, MoveitEnabledRobot):
 
-    def __init__(self, robot_namespace: str = 'med', force_trigger: float = -0.0, base_kwargs=None, **kwargs):
+    def __init__(self, robot_namespace: str = 'med', force_trigger: float = -0.0, base_kwargs=None, gripper_on=True, **kwargs):
         MoveitEnabledRobot.__init__(self,
                                     robot_namespace=robot_namespace,
                                     arms_controller_name='arm_trajectory_controller',
@@ -171,7 +172,8 @@ class Med(BaseMed, MoveitEnabledRobot):
         BaseMed.__init__(self, robot_namespace=robot_namespace, **base_kwargs)
         self.arm_group = 'kuka_arm'
         self.wrist = 'med_kuka_link_ee'
-        self.gripper = WSG50Gripper()
+        self.gripper_on = gripper_on
+        self.gripper = self._get_gripper()
 
     def get_arm_joints(self):
         return ARM_JOINT_NAMES
@@ -191,5 +193,12 @@ class Med(BaseMed, MoveitEnabledRobot):
 
     def release(self, width=110.0, speed=50.0):
         self.gripper.release(width=width, speed=speed)
+
+    def _get_gripper(self):
+        if self.gripper_on:
+            gripper = WSG50Gripper()
+        else:
+            gripper = FakeWSG50Gripper()
+        return gripper
 
 
