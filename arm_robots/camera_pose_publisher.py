fdea772e7b6ca07c143a501f5ab74b4af74675e9
Daniel Hou <houhd@umich.edu>
2025-06-16 22:45:08 -0400

    Added camera pose publisher

--- arm_robots/camera_pose_publisher.py
+++ arm_robots/camera_pose_publisher.py
@@ -14,11 +14,15 @@ def main():
     executor = MultiThreadedExecutor()
     executor.add_node(node)
     tfwrapper = TF2Wrapper(node)
-    calibrated_mat = np.array([[1.2519167e-01, 6.2875944e-01, - 7.6745594e-01, 1.5284070e+00],
-                      [9.9213248e-01, - 7.8984290e-02, 9.7132102e-02, - 5.0468093e-01],
-                      [4.5576331e-04, - 7.7357811e-01, - 6.3370085e-01, 1.1876023e+00],
-                      [0.0000000e+00, 0.0000000e+00, 0.0000000e+00, 1.0000000e+00]])
+    calibrated_mat = np.array([[ 0.15080321 , 0.45128095 ,-0.87954754 , 1.4242008 ],
+                             [ 0.9875272 , -0.0280358  , 0.15493222 , 0.36401126],
+                             [ 0.04525914 ,-0.8919414 , -0.4498801 ,  1.0284033 ],
+                             [ 0.      ,    0.    ,      0.        ,  1.        ]])
     tfwrapper.send_transform_matrix(calibrated_mat, "victor_root", CAMERA_FRAME, is_static=True)
+    mocap_vec = np.array([-0.6229922806800796, 0.6858175240681051, -0.6757562460396909,
+                          0.010570186931784202 ,-0.0035824702370211847 ,0.06951835956903875 ,
+                          0.9975182377972135])
+    tfwrapper.send_transform(mocap_vec[:3], mocap_vec[3:], "mocap_world", "victor_root", is_static=True)
     executor.spin()
 
 
